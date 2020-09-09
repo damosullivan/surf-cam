51f853d55be034ca6f44108ef7152210be8e854d
Damien <damosullivan@gmail.com>
2020-09-09 09:46:32 +0100

    Pasted a draft SurfCam.py I had created previously. Never run

--- SurfCam/modules/SurfCam.py
+++ SurfCam/modules/SurfCam.py
@@ -1,11 +1,47 @@
+from time import sleep
 
-class SurfCam:
+import cv2
 
-    CAMERA_IDENTIFIER = 1
+class SurfCam(object):
+    VIDEO_CAPTURE_ID = 0
 
-    def __init__(self, frequency: int, directory: str):
-        self._frequency = frequency
-        self._directory = directory
+    def __init__(self, frequency=10, ):
+        self.frequency = frequency
+        self.cam = cv2.VideoCapture(VIDEO_CAPTURE_ID)
 
-    def capture(self):
-        print("capturing")
+    def run(self):
+
+        # setup, verify credentials, log?
+
+        try:
+            while True:
+
+                image_name = self.captureImage()
+
+                # store image
+
+                # compress image
+
+                # upload image
+
+                sleep(self.frequency)
+            
+        except KeyboardInterrupt:
+            print("Killed!")
+        finally:
+            cv2.VideoCapture(self.VIDEO_CAPTURE_ID).release()
+
+        # terminated
+
+    def captureImage(self):
+        name = "test"
+        s, im = self.cam.read()
+        cv2.imwrite("{}.jpeg".format(name), im)
+        return name
+
+
+    def verifyAWS(self):
+        pass
+
+    def verifyDirecotry(self):
+        pass
