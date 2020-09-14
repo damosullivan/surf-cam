8cd24892c79fb9ee476f5998f27ddcd9b37d0f63
Damien O'Sullivan <damosullivan@gmail.com>
2020-09-14 21:31:48 +0100

    Reworked the load function. Not triggering

--- js/script.js
+++ js/script.js
@@ -1,7 +1,6 @@
 const IMAGE_BASE = "https://farranahown.s3-eu-west-1.amazonaws.com/";
 
-document.addEventListener("load", function(event) {
-
+const updateImage = () => {
     fetch("https://farranahown.com/.netlify/functions/latest")
         .then(data => {
             console.log(data);
@@ -14,8 +13,9 @@ document.addEventListener("load", function(event) {
             viewfinder.src = IMAGE_BASE + content.Key;
         })
         .catch(err => alert(err))
+};
 
-});
+document.addEventListener("load", updateImage);
 
 // https://farranahown.s3-eu-west-1.amazonaws.com/1599834372.jpeg
 // https://farranahown.s3-eu-west-1.amazonaws.com/1599834372.jpeg
