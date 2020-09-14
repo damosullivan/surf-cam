169f4396d642c5093792007c4463d60f485ab3c5
Damien O'Sullivan <damosullivan@gmail.com>
2020-09-14 21:24:17 +0100

    Added more logging in order to see whats up :/

--- js/script.js
+++ js/script.js
@@ -3,8 +3,13 @@ const IMAGE_BASE = "https://farranahown.s3-eu-west-1.amazonaws.com/";
 document.addEventListener("load", function(event) {
 
     fetch("https://farranahown.com/.netlify/functions/latest")
-        .then(data => data.json())
+        .then(data => {
+            console.log(data);
+            return data.json();
+        })
         .then(content => {
+            console.log(content);
+            
             let viewfinder = document.getElementById("viewfinder");
             viewfinder.src = IMAGE_BASE + content.Key;
         })
