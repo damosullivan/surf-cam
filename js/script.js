1bac170daecdd250f61fba24108037390ee45897
Damien O'Sullivan <damosullivan@gmail.com>
2020-09-14 21:07:01 +0100

    Added JS to load image from S3

---
+++ js/script.js
@@ -0,0 +1,12 @@
+const IMAGE_BASE = "https://farranahown.s3-eu-west-1.amazonaws.com/"
+
+document.addEventListener("DOMContentLoaded", function(event) {
+
+    fetch("https://farranahown.com/.netlify/functions/latest")
+        .then(data => data.json())
+        .then(content => {
+            let viewfinder = document.getElementById("viewfinder");
+            viewfinder.src = IMAGE_BASE + content.Key
+        }).catch(err => alert(err))
+
+});
