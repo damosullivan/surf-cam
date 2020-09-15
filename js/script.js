bc7d5ec07dc56d47bb3a0518a2e9ac9e17d2b412
Damien O'Sullivan <damosullivan@hubspot.com>
2020-09-15 11:12:11 +0100

    Ignore pyc files, returned a promist from function

--- js/script.js
+++ js/script.js
@@ -1,21 +1,18 @@
 const IMAGE_BASE = "https://farranahown.s3-eu-west-1.amazonaws.com/";
 
-const updateImage = () => {
-    fetch("https://farranahown.com/.netlify/functions/latest")
-        .then(data => {
-            console.log(data);
-            return data.json();
-        })
-        .then(content => {
-            console.log(content);
-            
-            let viewfinder = document.getElementById("viewfinder");
-            viewfinder.src = IMAGE_BASE + content.Key;
-        })
-        .catch(err => alert(err))
+const updateImage = async () => {
+  return fetch("https://farranahown.com/.netlify/functions/latest")
+    .then((data) => {
+      console.log(data);
+      return data.json();
+    })
+    .then((content) => {
+      console.log(content);
+
+      let viewfinder = document.getElementById("viewfinder");
+      viewfinder.src = IMAGE_BASE + content.Key;
+    })
+    .catch((err) => alert(err));
 };
 
-document.addEventListener("load", updateImage);
-
-// https://farranahown.s3-eu-west-1.amazonaws.com/1599834372.jpeg
-// https://farranahown.s3-eu-west-1.amazonaws.com/1599834372.jpeg
+document.addEventListener("DOMContentLoaded", updateImage);
