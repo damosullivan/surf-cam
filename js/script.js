b93b0a1e304313cd92f2c631845a490620cb5487
Damien O'Sullivan <damosullivan@hubspot.com>
2020-09-16 09:41:51 +0100

    Bypassing cache when fetching `latest`

--- js/script.js
+++ js/script.js
@@ -1,8 +1,8 @@
 const IMAGE_BASE = "https://farranahown.s3-eu-west-1.amazonaws.com/";
 
 const updateImage = async () => {
-  return fetch(IMAGE_BASE + "latest")
-    .then(data =>  data.text())
+  return fetch(IMAGE_BASE + "latest", { cache: "no-store" })
+    .then((data) => data.text())
     .then((image) => {
       let viewfinder = document.getElementById("viewfinder");
       viewfinder.src = IMAGE_BASE + image;
