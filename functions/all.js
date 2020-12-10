473008ce71d09aaeb999c1be4da8ae913df1ec47
Damien O'Sullivan <damosullivan@hubspot.com>
2020-12-10 16:26:04 +0000

    Just returning file name for simplicity

--- functions/all.js
+++ functions/all.js
@@ -14,10 +14,19 @@ exports.handler = async (event, context) => {
 
   return s3ListObject(s3, params)
     .then((data) => {
-      let contents = data.Contents;
+      let keys = data.Contents.map((content) => content.Key);
+
+      // {
+      //   "Key": "1607458428.jpeg",
+      //   "LastModified": "2020-12-08T20:13:49.000Z",
+      //   "ETag": "\"e7f76d892c8240e59501ef830c82fa55\"",
+      //   "Size": 76633,
+      //   "StorageClass": "STANDARD"
+      // }
+
       return {
         statusCode: 200,
-        body: JSON.stringify(contents),
+        body: JSON.stringify(keys),
       };
     })
     .catch((err) => {
